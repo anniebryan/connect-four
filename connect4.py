a6a8d42b74ef75103182ed1646023e672f735d71
--replace-all <annieb22@mit.edu>
2020-03-19 21:19:37 -0500

    implemented UI for visual gameplay with pygame

--- connect4.py
+++ connect4.py
@@ -1,5 +1,14 @@
 import numpy as np
 import pygame
+import sys
+
+BLUE = (14,54,134)
+RED = (157, 44, 32)
+YELLOW = (236,222,80)
+WHITE = (255, 25, 255)
+BLACK = (0,0,0)
+
+HEIGHT, WIDTH = 6,7
 
 def create_board(height, width):
     board = np.zeros((height, width))
@@ -99,7 +108,16 @@ def winner(board, row, col):
             
     return 0
 
-HEIGHT, WIDTH = 6,7
+def draw_board(board):
+    for c in range(WIDTH):
+        for r in range(HEIGHT):
+            pygame.draw.rect(screen, BLUE, (c * SQUARE_SIZE, (r+1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            if board[r][c] == 1:  color = RED
+            elif board[r][c] == 2: color = YELLOW
+            else: color = BLACK
+            
+            pygame.draw.circle(screen, color, (int((c + 0.5) * SQUARE_SIZE), int((r+1.5) * SQUARE_SIZE)), RADIUS)
+            
 board = create_board(HEIGHT, WIDTH)
 game_over = False
 turn = 1
@@ -111,13 +129,50 @@ SCREEN_WIDTH = WIDTH * SQUARE_SIZE
 SCREEN_HEIGHT = (HEIGHT+1) * SQUARE_SIZE 
 SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
-screen = pygame.display.set_mode(SIZE)
+RADIUS = int(0.8 * SQUARE_SIZE / 2)
 
-while not game_over:
-    board, row, col, turn = play_turn(turn)
-    print(board)
-    w = winner(board, row, col)
-    game_over = w != 0
-    if game_over:
-        text = ("Player {} wins!").format(w)
-        print(text)
+screen = pygame.display.set_mode(SIZE)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 75)
+
+while True:
+    for event in pygame.event.get():
+        if not game_over:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, BLACK, (0, 0, SCREEN_WIDTH, SQUARE_SIZE))
+                x, y = event.pos
+                if turn == 1: color = RED
+                else: color = YELLOW
+                pygame.draw.circle(screen, color, (x, int(SQUARE_SIZE/2)), RADIUS)
+            pygame.display.update()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                col = int(x / SQUARE_SIZE)
+                row = get_next_open_row(board, col)
+                drop_piece(board, row, col, turn)
+                draw_board(board)
+                turn = (turn%2)+1
+                w = winner(board, row, col)
+                game_over = w != 0   
+            pygame.display.update()
+        
+        else:
+            draw_board(board)
+            pygame.draw.rect(screen, BLACK, (0, 0, SCREEN_WIDTH, SQUARE_SIZE))
+            pygame.display.update()
+            if w == 1:
+                label = myfont.render("Player 1 wins!", 1, RED)
+            elif w == 2:
+                label = myfont.render("Player 2 wins!", 1, YELLOW)
+            else:
+                label = myfont.render("Tie game", 1, WHITE)
+            screen.blit(label, (40,10))
+            pygame.time.wait(3000)
+            pygame.display.update()
